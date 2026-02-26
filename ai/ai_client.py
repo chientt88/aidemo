75da7982b4aaab18074affd7875abf63e272f25f
chientt88 <112603875+chientt88@users.noreply.github.com>
2026-02-26 21:15:45 +0700

    Add files via upload

---
+++ ai/ai_client.py
@@ -0,0 +1,36 @@
+import requests
+from ai.prompt_templates import SYSTEM_PROMPT
+from memory.database import get_recent_messages
+
+API_KEY = "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"
+
+def call_ai(user_id, user_message):
+
+    history = get_recent_messages(user_id)
+
+    messages = [
+        {"role": "system", "content": SYSTEM_PROMPT},
+        *history,
+        {"role": "user", "content": user_message}
+    ]
+
+    data = {
+        "model": "llama-3.1-8b-instant",
+        "messages": messages
+    }
+    
+    url = "https://api.groq.com/openai/v1/chat/completions"
+
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json"
+    }
+
+    r = requests.post(url, headers=headers, json=data)
+
+    print("STATUS:", r.status_code)
+    print("RESPONSE:", r.text)
+
+    result = r.json()
+    return result["choices"][0]["message"]["content"]
+
---
+++ ai/prompt_templates.py
@@ -0,0 +1,8 @@
+SYSTEM_PROMPT = """
+Bạn là trợ lý gia đình thân thiện.
+- Nói chuyện tự nhiên như người Việt.
+- Quan tâm, không quá trang trọng.
+- Ghi nhớ thông tin người dùng nếu họ chia sẻ.
+- Nếu người dùng buồn, an ủi nhẹ nhàng.
+- Nếu là lệnh điều khiển thiết bị, trả lời ngắn gọn xác nhận.
+"""
