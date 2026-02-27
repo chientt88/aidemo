2050dde8b1f82fbff53467c521fcd5df1f3d3798
chientt88 <112603875+chientt88@users.noreply.github.com>
2026-02-27 06:59:54 -0800

    Add files via upload

--- bot/tele_bot.py
+++ bot/tele_bot.py
@@ -2,16 +2,18 @@
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 from intent.router import route_message
 from services.trade_watcher import watch_new_trades
+from services.ip_watcher import watch_ip_change
+import config
 
-BOT_TOKEN = "8584247201:AAGfYM_cHw0gYKSV5xW9hNpCT5dA9HOKjwI"
-OWNER_CHAT_ID = "1343641388"  # ← thay bằng chat_id của bạn
+BOT_TOKEN = config.get("bot_token")
+OWNER_CHAT_ID = config.get("owner_chat_id")
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_text = update.message.text
     user_id = str(update.effective_user.id)
 
-    print(f"[DEBUG] chat_id = {update.effective_chat.id}")  # ← xem trong terminal
+    print(f"[DEBUG] chat_id = {update.effective_chat.id}")
 
     response = route_message(user_id, user_text)
     await update.message.reply_text(response)
@@ -20,6 +22,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def on_startup(app):
     print("[Bot] Khởi động watcher...")
     app.create_task(watch_new_trades(app.bot, OWNER_CHAT_ID, interval=10))
+    app.create_task(watch_ip_change(app.bot, OWNER_CHAT_ID, interval=60))
 
 
 def start_bot():
