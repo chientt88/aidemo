48e64fc6f4524b52b66ad113759ba89863ab1cab
chientt88 <112603875+chientt88@users.noreply.github.com>
2026-02-27 02:24:45 -0800

    Add files via upload

--- bot/tele_bot.py
+++ bot/tele_bot.py
@@ -1,19 +1,28 @@
-from telegram import Update
+﻿from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 from intent.router import route_message
+from services.trade_watcher import watch_new_trades
 
-BOT_TOKEN = ""
+BOT_TOKEN = "8584247201:AAGfYM_cHw0gYKSV5xW9hNpCT5dA9HOKjwI"
+OWNER_CHAT_ID = "1343641388"  # ← thay bằng chat_id của bạn
+
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_text = update.
+    user_text = update.message.text
     user_id = str(update.effective_user.id)
 
-    response = route_message(user_id, user_text)
+    print(f"[DEBUG] chat_id = {update.effective_chat.id}")  # ← xem trong terminal
 
+    response = route_message(user_id, user_text)
     await update.message.reply_text(response)
 
-def start_bot():
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
+async def on_startup(app):
+    print("[Bot] Khởi động watcher...")
+    app.create_task(watch_new_trades(app.bot, OWNER_CHAT_ID, interval=10))
+
+
+def start_bot():
+    app = ApplicationBuilder().token(BOT_TOKEN).post_init(on_startup).build()
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.run_polling()
