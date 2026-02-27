9b6148afa0410e504519455fe5dcb2f9f57e1805
chientt88 <112603875+chientt88@users.noreply.github.com>
2026-02-27 02:24:19 -0800

    Add files via upload

--- ai/ai_client.py
+++ ai/ai_client.py
@@ -1,31 +1,30 @@
 import requests
 from ai.prompt_templates import SYSTEM_PROMPT
 from memory.database import get_recent_messages
+from ai.prompt_templates import TOOL_DECIDER_PROMPT
 
-API_KEY = ""
-
+API_KEY = "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"
+url = "https://api.groq.com/openai/v1/chat/completions"
+headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json"
+    }
+# llama-3.3-70b-versatile
+# llama-3.1-8b-instant
 def call_ai(user_id, user_message):
-
     history = get_recent_messages(user_id)
-
     messages = [
         {"role": "system", "content": SYSTEM_PROMPT},
         *history,
         {"role": "user", "content": user_message}
     ]
-
     data = {
-        "model": "llama-3.1-8b-instant",
+        "model": "llama-3.3-70b-versatile",
         "messages": messages
-    }
+    }    
     
-    url = "https://api.groq.com/openai/v1/chat/completions"
 
-    headers = {
-        "Authorization": f"Bearer {API_KEY}",
-        "Content-Type": "application/json"
-    }
-
+  
     r = requests.post(url, headers=headers, json=data)
 
     print("STATUS:", r.status_code)
@@ -33,5 +32,41 @@ def call_ai(user_id, user_message):
 
     result = r.json()
     return result["choices"][0]["message"]["content"]
+    
+    
+import json
+from core.tools import TOOLS
+def decide_tool(user_message):
 
+    tool_descriptions = ""
 
+    for name, tool in TOOLS.items():
+        tool_descriptions += f"""
+Tool name: {name}
+Description: {tool['description']}
+"""
+
+    system_prompt = TOOL_DECIDER_PROMPT + "\nAvailable tools:\n" + tool_descriptions
+
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_message}
+    ]
+
+    data = {
+        "model": "llama-3.3-70b-versatile",
+        "messages": messages,
+        "temperature": 0
+    }
+
+    r = requests.post(url, headers=headers, json=data)
+    result = r.json()
+
+    content = result["choices"][0]["message"]["content"]
+
+    print("TOOL RAW:", content)
+
+    try:
+        return json.loads(content)
+    except:
+        return {"tool": None}
--- ai/prompt_templates.py
+++ ai/prompt_templates.py
@@ -6,3 +6,17 @@ Bạn là trợ lý gia đình thân thiện.
 - Nếu người dùng buồn, an ủi nhẹ nhàng.
 - Nếu là lệnh điều khiển thiết bị, trả lời ngắn gọn xác nhận.
 """
+
+TOOL_DECIDER_PROMPT = """
+Bạn là AI phân loại yêu cầu. Chỉ trả về JSON thuần, không giải thích.
+
+Available tools sẽ được liệt kê bên dưới. Nếu user hỏi liên quan đến tool đó, trả về đúng tên tool.
+
+Ví dụ output khi là tool:
+{"type": "tool", "tool": "<tên_tool>"}
+
+Ví dụ output khi là chat thường:
+{"type": "chat", "tool": null}
+
+Chỉ trả JSON. Không markdown. Không giải thích.
+"""
